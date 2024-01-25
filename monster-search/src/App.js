1b49d58305dbf15a180951150775bfd14be5dd88
puranik <puranik@mymacewan.ca>
2024-01-25 13:25:48 -0700

    First React Application

---
+++ monster-search/src/App.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import './App.css';
+import { SearchBar } from "./components/searchbar/searchbar.component";
+import { CardList } from "./components/cardList/cardlist.component";
+function App() {
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState([]);
+  const [searchInput, setSearchInput] = useState("")
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await fetch("https://jsonplaceholder.typicode.com/users");
+      const users = await response.json();
+      setMonsters(users);
+    };
+    fetchUsers();
+  }, []);
+  useEffect(() => {
+    let filtered = [];
+    if (searchInput === "") {
+    filtered = monsters
+    } else {
+    filtered = monsters.filter(monster =>
+    monster.name.toLowerCase().includes(searchInput.toLowerCase())
+    );
+    }
+    setFilteredMonsters(filtered);
+  }, [monsters, searchInput]);
+  const handleInput = e => {
+    setSearchInput(e.target.value)
+  };
+  return (
+    <div className="App">
+      <h1>Monster Rolodex</h1>
+      <SearchBar
+      placeholder='Search Monster'
+      handleInput={handleInput}
+      />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
+
+export default App;
