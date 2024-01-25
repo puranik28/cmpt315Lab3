73a1951c4a544f33e5147c7ba03ac0fb8d2453ed
puranik <puranik@mymacewan.ca>
2024-01-25 13:35:45 -0700

    Using Axios instead of Fetch

--- monster-search/src/App.js
+++ monster-search/src/App.js
@@ -2,17 +2,21 @@ import React, { useEffect, useState } from "react";
 import './App.css';
 import { SearchBar } from "./components/searchbar/searchbar.component";
 import { CardList } from "./components/cardList/cardlist.component";
+import axios from 'axios';
+
 function App() {
   const [monsters, setMonsters] = useState([]);
   const [filteredMonsters, setFilteredMonsters] = useState([]);
   const [searchInput, setSearchInput] = useState("")
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const response = await fetch("https://jsonplaceholder.typicode.com/users");
-      const users = await response.json();
-      setMonsters(users);
-    };
-    fetchUsers();
+  
+useEffect(() => {
+  const fetchUsers = async () => {
+    const response = await axios(
+    'https://jsonplaceholder.typicode.com/users',
+    );
+    setMonsters(response.data);
+  };
+  fetchUsers();
   }, []);
   useEffect(() => {
     let filtered = [];
@@ -30,7 +34,7 @@ function App() {
   };
   return (
     <div className="App">
-      <h1>Monster Rolodex</h1>
+      <h1>Monster World</h1>
       <SearchBar
       placeholder='Search Monster'
       handleInput={handleInput}
